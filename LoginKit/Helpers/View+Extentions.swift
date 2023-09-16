c4b0c8f5fe036ba2b5b99b827fbf46b5817c7e49
rvshah2217 <rvshah2217@gmail.com>
2023-09-16 14:44:28 +0530

    Chore: Added View Builders Or Alignment With Disable Screen Opacity

---
+++ LoginKit/Helpers/View+Extentions.swift
@@ -0,0 +1,28 @@
+//
+//  View+Extentions.swift
+//  LoginKit
+//
+//  Created by Ravi Shah on 16/09/23.
+//
+
+import SwiftUI
+
+extension View {
+    
+    //MARK: View Alignment
+    @ViewBuilder
+    func hSpacing(_ alignment: Alignment = .center) -> some View {
+        self.frame(minWidth: .infinity, alignment: alignment)
+    }
+    
+    @ViewBuilder
+    func vSpacing(_ alignment: Alignment = .center) -> some View {
+        self.frame(maxHeight: .infinity, alignment: alignment)
+    }
+    //MARK: Disable With Opacity
+    func disableWithOpacity(_ Condition : Bool) -> some View {
+        self.disabled(Condition)
+            .opacity(Condition ? 0.5 :  1)
+    }
+    
+}
