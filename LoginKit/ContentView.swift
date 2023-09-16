52d0227c90191053ffeca37589affd32544d0381
rvshah2217 <rvshah2217@gmail.com>
2023-09-16 22:17:25 +0530

    Link Signup To MainView

--- LoginKit/ContentView.swift
+++ LoginKit/ContentView.swift
@@ -7,16 +7,46 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    
+    @State private var showSignup: Bool = false
+    //MARK: Main View Link With NavigationView
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack{
+            Login(showSignup: $showSignup)
+                .navigationDestination(isPresented: $showSignup, destination: {
+                    Signup(showSignup: $showSignup)
+                })
         }
-        .padding()
+        .overlay{
+            if #available(iOS 17, *){
+                //MARK: This Support iOS 17 And 16 Too
+                CircleView()
+                    .animation(.easeOut(duration: 0.3), value: showSignup)
+            }else{
+                CircleView()
+                    .animation(.easeOut(duration: 0.3), value: showSignup)
+            }
+        }
     }
+    //MARK: Stack End Here
+    
+    //MARK: Moving Blur Effect or Background
+    
+    @ViewBuilder
+    func CircleView() -> some View {
+        
+        Circle()
+            .fill(.linearGradient(colors: [.accentColor, .teal], startPoint: .top, endPoint: .bottom))
+            .frame(width: 200, height: 200)
+        //MARK: Moving When SignupPage Dispear or Load
+            .offset(x: showSignup ? 90 : -90, y: -90)
+            .blur(radius: 10)
+            .hSpacing(showSignup ? .trailing : .leading)
+            .vSpacing(.top)
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
