dd13808c1fc1d06ec67e74e29871145ab50b946b
rvshah2217 <rvshah2217@gmail.com>
2023-09-16 14:18:21 +0530

    Initial Commit

---
+++ LoginKit/LoginKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginKitApp.swift
+//  LoginKit
+//
+//  Created by Ravi Shah on 16/09/23.
+//
+
+import SwiftUI
+
+@main
+struct LoginKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
